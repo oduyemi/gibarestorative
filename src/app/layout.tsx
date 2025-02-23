e482c93ffd27df0ec84bba85ac0d4a5f3740a657
Opeyemi Oduyemi <119660096+oduyemi@users.noreply.github.com>
2025-02-23 19:04:07 +0100

    Meta description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Box, ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
 import "./globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "animate.css";
@@ -10,13 +11,19 @@ import { Footer } from "@/navigation/Footer";
 
 export default function RootLayout({
   children,
-  hideLayout = false, // Default is false
+  hideLayout = false,
 }: {
   children: React.ReactNode;
   hideLayout?: boolean;
 }) {
   return (
     <html lang="en">
+      <Head>
+        <title>Giba Restorative Clinic</title>
+        <meta name="description" content="Best Weight Loss Clinic" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body style={{ margin: 0, padding: 0 }}>
         <ChakraProvider>
           <Box minH="100vh" display="flex" flexDirection="column" m={0} p={0}>
